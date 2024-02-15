1469753aff818146bee2be4967dd36365ca1b11b
Mahmodul Hasan <nicesanto92@gmail.com>
2024-02-15 20:41:06 +0600

    initialized prisma in to the project and  using vercel postgres store as database

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,11 @@
 import { ToggleMode } from "@/components/toggle-mode";
 import { UserButton } from "@clerk/nextjs";
- 
+
 export default function Home() {
-  return (
-    <div className="h">
-      <UserButton afterSignOutUrl="/"/>
-      <ToggleMode/>
-    </div>
-  )
-} 
+	return (
+		<div className="">
+			<UserButton afterSignOutUrl="/" />
+			<ToggleMode />
+		</div>
+	);
+}
