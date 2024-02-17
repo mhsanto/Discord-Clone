261f0aeee5f7598eb0a6a5d3d07367fb6e41029d
Mahmodul Hasan <nicesanto92@gmail.com>
2024-02-17 18:36:34 +0600

    Server modal initial design is complete beside image upload feature

--- components/modal/initial-modal.tsx
+++ components/modal/initial-modal.tsx
@@ -4,6 +4,7 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "../ui/dialog";
@@ -17,12 +18,18 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 export const InitialModal = () => {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   const formSchema = z.object({
-    name: z.string().min(1, { message: "Please enter a server name." }),
+    name: z.string().min(1, { message: "Please give your server a name." }),
     imageUrl: z.string().min(1, { message: "Please enter a server image." }),
   });
 
@@ -33,24 +40,71 @@ export const InitialModal = () => {
       imageUrl: "",
     },
   });
-const loading = form.formState.isSubmitting;
+  const loading = form.formState.isSubmitting;
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+  if (!mounted) return null;
   return (
     <Dialog open>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
-        <DialogHeader className="pt-8 px-6">
-          <DialogTitle className="text-2xl text-center font-bold">
-            Create your first server
+      <DialogContent className="bg-white text-black dark:text-white dark:bg-zinc-800   overflow-hidden max-w-sm">
+        <DialogHeader className="p-2">
+          <DialogTitle className="text-2xl text-center font-bold text-black dark:text-white">
+            Create Your First Server
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500 ">
-            Give your server a name and invite your friends to start chatting.
+          <DialogDescription className="text-center text-zinc-500 dark:text-zinc-400 ">
+            Give your server a personality with a name and an icon.You can
+            always change it later.
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="flex items-center justify-center text-center">
+              TODO:Image upload
+            </div>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="uppercase text-gray-200 text-xs">
+                    Server name
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Developer's are here"
+                      {...field}
+                      className="dark:bg-zinc-900  "
+                    />
+                  </FormControl>
+                  <FormDescription className="dark:text-zinc-400">
+                    By creating a server, you agree to Discord&apos;s{" "}
+                    <Link
+                      href="/community"
+                      className="text-indigo-400 font-medium"
+                    >
+                      Community Guidelines
+                    </Link>
+                  </FormDescription>
+                  <FormMessage className="text-red-600" />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button
+                type="submit"
+                disabled={loading}
+                variant="primary"
+                className="w-full"
+              >
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
