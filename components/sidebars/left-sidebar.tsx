38b9565b6737e93fede6de143dfaaf7a5231d956
Mahmodul Hasan <nicesanto92@gmail.com>
2024-02-20 20:11:53 +0600

    added scroll area component from shadcn  for show list of servers

--- components/sidebars/left-sidebar.tsx
+++ components/sidebars/left-sidebar.tsx
@@ -1,13 +1,15 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { Navigation } from "./navigation";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
 
 export const LeftSidebar = async () => {
   const profile = await currentProfile();
 
   if (!profile) throw new Error("Please Sign up with your email");
 
-  const server = await db.server.findMany({
+  const servers = await db.server.findMany({
     where: {
       members: {
         some: {
@@ -20,6 +22,12 @@ export const LeftSidebar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1f1f22] py-3">
       <Navigation />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea>
+        {servers.map((server) => (
+          <div key={server.id}>{server.name}</div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
